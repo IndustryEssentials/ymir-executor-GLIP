fc193a6d22fd5693bb439a8fa348104e9c135565
ymir <ymir@intellif.com>
2023-06-04 21:34:46 +0800

    checkout ymir/util.py from prima/dev

--- ymir/util.py
+++ ymir/util.py
@@ -1,7 +1,6 @@
 from ymir_exc.code import ExecutorState, ExecutorReturnCode
 from ymir_exc import monitor
 import urllib
-from easydict import EasyDict as edict
 import os, shutil
 import json,yaml
 import logging
@@ -11,48 +10,54 @@ import numpy as np
 import torch.utils.data as td
 from maskrcnn_benchmark.config import cfg
 from maskrcnn_benchmark.utils.miscellaneous import  save_config
-from pycocotools import mask as maskUtils
 
 
-def split_into_train_val(index_file,imgdir,output_json_file):
+def _read_coco_and_fix_ids(coco_file: str) -> dict:
+    """ coco wants category ids from 1, but ymir from 0 """
+    with open(coco_file, 'r') as f:
+        coco_data = json.load(f)
+    for cat in coco_data['categories']:
+        cat['id'] += 1
+    for anno in coco_data['annotations']:
+        anno['category_id'] += 1
+    return coco_data
+
+
+def _split_into_train_val(index_file: str, out_img_dir: str, output_json_file: str, coco_data: dict) -> None:
     '''
     /out/tmp/assets/2e/b1a44898eefd6e08ee28e455ab046187eaed1a2e.png      /out/tmp/annotations/coco-annotations.json
     '''
 
-    if not os.path.isdir(imgdir):
-        logging.info(f'make dir for train image in {imgdir}')
-        os.makedirs(imgdir)
+    if not os.path.isdir(out_img_dir):
+        logging.info(f'make dir for train image in {out_img_dir}')
+        os.makedirs(out_img_dir)
 
     img_names = []
     with open(index_file,'r') as f:
         for line in f.readlines():
-            img_path, input_json_file = line.strip().split()
-            shutil.copy(img_path,imgdir)
+            img_path, *_ = line.strip().split()
+            shutil.copy(img_path, out_img_dir)
             img_name = os.path.basename(img_path)
             img_names.append(img_name)
 
-    with open(input_json_file, 'r') as f:
-        data = json.load(f)
-
-    new_data = {
+    out_coco_data = {
         'images': [],
         'annotations': [],
-        'categories': data['categories']
+        'categories': coco_data['categories']
     }
 
     image_ids = set()
-    for image in data['images']:
+    for image in coco_data['images']:
         if image['file_name'] in img_names:
-            new_data['images'].append(image)
+            out_coco_data['images'].append(image)
             image_ids.add(image['id'])
 
-    for annotation in data['annotations']:
+    for annotation in coco_data['annotations']:
         if annotation['image_id'] in image_ids:
-            new_data['annotations'].append(annotation)
+            out_coco_data['annotations'].append(annotation)
 
     with open(output_json_file, 'w') as f:
-        json.dump(new_data, f)
-    return new_data['categories']
+        json.dump(out_coco_data, f)
 
 
 def create_ymir_dataset_config(ymir_cfg):
@@ -74,8 +79,16 @@ def create_ymir_dataset_config(ymir_cfg):
     val_output_json_file='/out/tmp/val/val.json'
     train_output_json_file='/out/tmp/train/train.json'
 
-    dataset_category = split_into_train_val(training_index_file,split_train_img_dir,train_output_json_file)
-    _ = split_into_train_val(val_index_file,split_val_img_dir,val_output_json_file)
+    coco_data = _read_coco_and_fix_ids(coco_file='/in/annotations/coco-annotations.json')
+    dataset_category = coco_data['categories']
+    _split_into_train_val(index_file=training_index_file,
+                          out_img_dir=split_train_img_dir,
+                          output_json_file=train_output_json_file,
+                          coco_data=coco_data)
+    _split_into_train_val(index_file=val_index_file,
+                          out_img_dir=split_val_img_dir,
+                          output_json_file=val_output_json_file,
+                          coco_data=coco_data)
 
     YMIR_DATASET = dict()
     DATASETS = dict()
@@ -168,6 +181,7 @@ def process_error(e,msg='defult'):
 
 
 def load_image_file(img_path):
+
     pil_image = Image.open(img_path).convert("RGB")
     # convert to BGR format
     image = np.array(pil_image)[:, :, [2, 1, 0]]
@@ -202,7 +216,7 @@ def get_weight_file(ymir_cfg):
     return ''
 
 
-def gen_anns_from_dets(top_predictions, ymir_infer_result, caption, img_path):
+def gen_anns_from_dets(top_predictions,ymir_infer_result,caption,img_path):
     """Generates json annotations from detections."""
 
     # Load the detections
@@ -230,7 +244,7 @@ def gen_anns_from_dets(top_predictions, ymir_infer_result, caption, img_path):
     if 'images' in ymir_infer_result:
         ymir_infer_result['images'].append(img)
     else:
-        ymir_infer_result['images'] = [img]
+        ymir_infer_result['images']=[img]
 
     caption= caption.split(' . ')
     for i in range(len(caption)):
